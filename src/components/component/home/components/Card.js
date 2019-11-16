1b54e95581b6315fb08ed1be97099045ccea57b7
williamchand <williamchans57@gmail.com>
2019-11-16 19:29:36 +0700

    feat(recipe-airy): Card Component logic

--- src/components/component/home/components/Card.js
+++ src/components/component/home/components/Card.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function Card(props) {
+  const {image, title, rating, description} = props;
   return (
     <div class="card">
+      <img class="card-img-top" src={image} alt="" />
+      <div>{rating}</div>
+      <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{description}</p>
+      </div>
     </div>
   );
 }
