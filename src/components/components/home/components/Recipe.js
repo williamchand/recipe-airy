9d21b8633413338516a2d34dfbea22f1958e710e
williamchand <williamchans57@gmail.com>
2019-11-17 10:51:44 +0700

    feat: api fetch

--- src/components/components/home/components/Recipe.js
+++ src/components/components/home/components/Recipe.js
@@ -3,7 +3,24 @@ import Card from './Card';
 import {uRecipe} from './styles';
 
 function Recipe() {
-  
+  const [recipeList, setRecipeList] = React.useState([]);
+  const recipeListFiltered = recipeList
+    .filter(
+      elem => elem.ingredients.find(
+        val => val === "Chicken"
+      )
+    )
+    .sort(
+      (a, b) => b.rating - a.rating
+    );
+  React.useEffect(()=>{
+    fetch("https://api.myjson.com/bins/171v5n").then(res=> res.json())
+      .then(response => setRecipeList(response.recipe))
+      .catch(()=>{
+        setRecipeList([]);
+      });
+  },[setRecipeList]);
+  console.log(recipeListFiltered);
   return (
     <div className={uRecipe}>
       {/* <Card image={image} title={title} rating={rating} description={description} creator={creator} /> */}
